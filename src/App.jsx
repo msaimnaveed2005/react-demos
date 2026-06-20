b610e828a778a57fea6209016b53be90a8aee250
Muhammad Saim Naveed <msaimnaveed5384@gmail.com>
2026-06-20 17:25:48 +0500

    Complete Task 3 Showing JSX Rules through components

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import Footer from "./components/Footer";
 import Greek from "./components/Greek";
 import Greeting from "./components/Greeting";
 import Header from "./components/Header";
+import JSXRules from "./components/JSXRules";
 import MainContent from "./components/MainContent";
 import WelcomeMessage from "./components/WelcomeMessage";
 function App(params)
 {
   return (
-    <WelcomeMessage />
+    <JSXRules />
   );
 }
 export default App;
---
+++ src/components/JSXRules.jsx
@@ -0,0 +1,13 @@
+const JSXRules = () => {
+  return (
+    <div>
+        <h1>JSX Rules</h1>
+        <p>1. JSX must return a single parent element</p>
+        <p>2. Use camelCase for HTML attributes</p>
+        <p>3. Class becomes className</p>
+        <p>4. JSX can contain JavaScript expressions</p>
+    </div>
+  )
+}
+
+export default JSXRules
